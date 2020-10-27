7ab125b30842e5a946698833df0e64339223514e
Yuki Akahori <y.akahori.ramen@gmail.com>
2020-10-27 09:12:29 +0900

    テストケースモデルの作成

--- service/app/testhosting/hosting/models.py
+++ service/app/testhosting/hosting/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class TestCase(models.Model):
+    """テストケースモデル
+
+    Args:
+        title_path (models.CharField): テストケースパス名
+        summary (models.CharField): テストケースの概要
+        testcase_data (models.CharField): テストケースのテキストデータ
+    """
+    title_path = models.CharField(max_length=50)
+    summary = models.TextField()
+    testcase_data = models.TextField()
+
+    def __str__(self):
+        return f'{self.title_path}'
